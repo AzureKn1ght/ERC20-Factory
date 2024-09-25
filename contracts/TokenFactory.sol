5db90e5972de217ff12bfe028e38c9a51a9d6df5
AzureKn1ght <philsonnah@msn.com>
2024-09-25 13:54:58 +0800

    Added factory contract

---
+++ contracts/TokenFactory.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+import "./MemeToken.sol";
+
+contract TokenFactory {
+    address[] createdTokens;
+    event TokenCreated(address tokenAddress);
+
+    function createToken(
+        string calldata _name,
+        string calldata _ticker
+    ) public returns (address) {
+        MemeToken token = new MemeToken(msg.sender, _name, _ticker);
+        createdTokens.push(address(token));
+        emit TokenCreated(address(token));
+        return address(token);
+    }
+
+    function numTokens() public view returns (uint256) {
+        return createdTokens.length;
+    }
+}
